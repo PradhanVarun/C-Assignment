96fb2f514d546135ad63e20f0d174db23ca49f15
PradhanVarun <133233624+PradhanVarun@users.noreply.github.com>
2023-05-18 18:02:13 +0530

    Add files via upload

---
+++ DisplayInfo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+
+{
+	printf("Name:\t\tVarun Shripad Pradhan\n");
+	printf("Birth Date:\t22nd June 1998\n");
+	printf("Age:\t\t24\n");
+	printf("Address:\tA-202, Haveli Harmony Flats,\n\t\tHarinagar,\n\t\tGotri,\n\t\tVadodara");
+	
+}
---
+++ square_cube.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b, square, cube;
+	
+		printf("Enter the number a: ",a);
+	    scanf("%d", &a);
+    	square=a*a;
+    	printf("%d*%d=%d\n", a,a,square);
+	
+		printf("Enter the number b: ",b);
+	    scanf("%d", &b);
+    	cube=b*b*b;
+    	printf("%d*%d*%d=%d", b,b,b,cube);
+	
+	
+}
